755ac59a649ffa523c9dc65669c08f3dbd1c9883
EricLScace <mail@ericscace.com>
2017-07-19 19:41:20 -0400

    Create route & template for books
    Tests: OK
    
    Issue #6

--- app/router.js
+++ app/router.js
@@ -1,15 +1,16 @@
-import Ember from 'ember';
-import config from './config/environment';
+import Ember from 'ember'
+import config from './config/environment'
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-});
+})
 
 Router.map(function () {
-  this.route('sign-up');
-  this.route('sign-in');
-  this.route('change-password');
-  this.route('users');
-});
+  this.route('books')
+  this.route('sign-up')
+  this.route('sign-in')
+  this.route('change-password')
+  this.route('users')
+})
 
-export default Router;
+export default Router

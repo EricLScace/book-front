e458f1ba9d4a9b8f62cbdc89705acf71778a7376
EricLScace <mail@ericscace.com>
2017-07-20 10:24:08 -0400

    Rename a-book as book-list/book component
    Test: needs code yet.
    
    Issue #18

--- tests/integration/components/book-list/book-test.js
+++ tests/integration/components/book-list/book-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('a-book', 'Integration | Component | a book', {
+moduleForComponent('book-list/book', 'Integration | Component | book list/book', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{a-book}}`);
+  this.render(hbs`{{book-list/book}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#a-book}}
+    {{#book-list/book}}
       template block text
-    {{/a-book}}
+    {{/book-list/book}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');

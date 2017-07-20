79512b73d34c18f62575c00d7d67085a6e88746c
EricLScace <mail@ericscace.com>
2017-07-20 10:05:06 -0400

    Render a hard-code list of books
    Test: ok
    
    Closes #16
    Issue #15
    Issue #13

--- app/routes/book-list.js
+++ app/routes/book-list.js
@@ -1,4 +1,12 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Route.extend({
-});
+  model () {
+    return [
+      {
+        title: 'The Hound of the Baskervilles',
+        author: 'Arthur Conan Doyle'
+      }
+    ]
+  }
+})
